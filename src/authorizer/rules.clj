0457aa2819549997b0795ea9fff4403a5f89b525
Wennder dos Santos <wennder.santos@outlook.com>
2020-10-07 01:17:56 -0300

    feat: add highFrequencySmallInterval? rule

--- src/authorizer/rules.clj
+++ src/authorizer/rules.clj
@@ -1,4 +1,5 @@
-(ns authorizer.rules)
+(ns authorizer.rules
+  (:require [clj-time.core :as t]))
 
 (defn hasLimit?
   "checks if an account has sufficient limit to complete a transaction"
@@ -10,3 +11,12 @@
    otherwhise false"
    [account]
    (true? (:activeCard account)))
+
+(defn highFrequencySmallInterval?
+  [transactions]
+  (let [intervalInMinutes 2
+        highFrequencyParameterCount 3
+        dateTimeInterval (t/minus (t/now) (t/minutes intervalInMinutes))]
+    (-> (filter #(t/within? dateTimeInterval (t/now) (:time %)) transactions)
+        count
+        (> highFrequencyParameterCount))))
--- test/authorizer/rules_test.clj
+++ test/authorizer/rules_test.clj
@@ -1,6 +1,7 @@
 (ns authorizer.rules_test
   (:require [midje.sweet :refer :all]
-            [authorizer.rules :as rules]))
+            [authorizer.rules :as rules]
+            [clj-time.core :as t]))
 
 (fact "hasLimit?"
   (fact "given account limit >= transaction amount should return true"
@@ -15,3 +16,29 @@
 
   (fact "given card is not active should return false"
     (rules/activeCard? {:activeCard false}) => false))
+
+(fact "highFrequencySmallInterval?"
+  (fact "given there are more than 3 transactions in the last 2 minutes should return true"
+    (rules/highFrequencySmallInterval? [{:time (t/now)}
+                                        {:time (t/now)}
+                                        {:time (t/now)}
+                                        {:time (t/now)}]) => true
+
+    (rules/highFrequencySmallInterval? [{:time (t/minus (t/now) (t/minutes 1))}
+                                        {:time (t/minus (t/now) (t/minutes 2))}
+                                        {:time (t/minus (t/now) (t/minutes 2))}
+                                        {:time (t/now)}]) => true)
+
+  (fact "given there are less or equal to 3 transactions in the last 2 minutes should return false"
+    (rules/highFrequencySmallInterval? [{:time (t/minus (t/now) (t/minutes 4))}
+                                        {:time (t/minus (t/now) (t/minutes 3))}
+                                        {:time (t/minus (t/now) (t/minutes 5))}
+                                        {:time (t/now)}]) => false
+
+    (rules/highFrequencySmallInterval? [{:time (t/minus (t/now) (t/minutes 20))}
+                                        {:time (t/minus (t/now) (t/minutes 3))}
+                                        {:time (t/minus (t/now) (t/minutes 2))}
+                                        {:time (t/minus (t/now) (t/minutes 5))}
+                                        {:time (t/now)}]) => false))
+
+
