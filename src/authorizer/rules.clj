13fb420c268c0ec3209c45613af233499149c79c
Wennder dos Santos <wennder.santos@outlook.com>
2020-10-07 23:14:26 -0300

    refact: using snake-case instead of camelCase

--- src/authorizer/rules.clj
+++ src/authorizer/rules.clj
@@ -1,22 +1,22 @@
 (ns authorizer.rules
   (:require [clj-time.core :as t]))
 
-(defn hasLimit?
+(defn has-limit?
   "checks if an account has sufficient limit to complete a transaction"
   [account transaction-amount]
   (true? (>= (:limit account) transaction-amount)))
 
-(defn activeCard?
+(defn active-card?
   "return true if a card is active
    otherwhise false"
    [account]
-   (true? (:activeCard account)))
+   (true? (:active-card account)))
 
-(defn highFrequencySmallInterval?
+(defn high-frequency-small-interval?
   [transactions]
-  (let [intervalInMinutes 2
-        highFrequencyParameterCount 3
-        dateTimeInterval (t/minus (t/now) (t/minutes intervalInMinutes))]
-    (-> (filter #(t/within? dateTimeInterval (t/now) (:time %)) transactions)
+  (let [interval-in-minutes 2
+        high-frequency-transaction-count 3
+        date-time-start (t/minus (t/now) (t/minutes interval-in-minutes))]
+    (-> (filter #(t/within? date-time-start (t/now) (:time %)) transactions)
         count
-        (> highFrequencyParameterCount))))
+        (> high-frequency-transaction-count))))
--- test/authorizer/rules_test.clj
+++ test/authorizer/rules_test.clj
@@ -3,39 +3,39 @@
             [authorizer.rules :as rules]
             [clj-time.core :as t]))
 
-(fact "hasLimit?"
+(fact "has-limit?"
   (fact "given account limit >= transaction amount should return true"
-    (rules/hasLimit? {:limit 100} 90) => true)
+    (rules/has-limit? {:limit 100} 90) => true)
 
   (fact "given account limit < transaction amount should return false"
-    (rules/hasLimit? {:limit 10} 90) => false))
+    (rules/has-limit? {:limit 10} 90) => false))
 
-(fact "activeCard?"
+(fact "active-card?"
   (fact "given card is active should return true"
-    (rules/activeCard? {:activeCard true}) => true)
+    (rules/active-card? {:active-card true}) => true)
 
   (fact "given card is not active should return false"
-    (rules/activeCard? {:activeCard false}) => false))
+    (rules/active-card? {:active-card false}) => false))
 
-(fact "highFrequencySmallInterval?"
+(fact "high-frequency-small-interval?"
   (fact "given there are more than 3 transactions in the last 2 minutes should return true"
-    (rules/highFrequencySmallInterval? [{:time (t/now)}
+    (rules/high-frequency-small-interval? [{:time (t/now)}
                                         {:time (t/now)}
                                         {:time (t/now)}
                                         {:time (t/now)}]) => true
 
-    (rules/highFrequencySmallInterval? [{:time (t/minus (t/now) (t/minutes 1))}
+    (rules/high-frequency-small-interval? [{:time (t/minus (t/now) (t/minutes 1))}
                                         {:time (t/minus (t/now) (t/minutes 2))}
                                         {:time (t/minus (t/now) (t/minutes 2))}
                                         {:time (t/now)}]) => true)
 
   (fact "given there are less or equal to 3 transactions in the last 2 minutes should return false"
-    (rules/highFrequencySmallInterval? [{:time (t/minus (t/now) (t/minutes 4))}
+    (rules/high-frequency-small-interval? [{:time (t/minus (t/now) (t/minutes 4))}
                                         {:time (t/minus (t/now) (t/minutes 3))}
                                         {:time (t/minus (t/now) (t/minutes 5))}
                                         {:time (t/now)}]) => false
 
-    (rules/highFrequencySmallInterval? [{:time (t/minus (t/now) (t/minutes 20))}
+    (rules/high-frequency-small-interval? [{:time (t/minus (t/now) (t/minutes 20))}
                                         {:time (t/minus (t/now) (t/minutes 3))}
                                         {:time (t/minus (t/now) (t/minutes 2))}
                                         {:time (t/minus (t/now) (t/minutes 5))}
