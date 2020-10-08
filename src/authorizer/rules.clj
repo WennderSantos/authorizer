91a2c42a5c96d930a6dadb8eb698d5ca2cfe23ff
Wennder dos Santos <wennder.santos@outlook.com>
2020-10-07 23:28:39 -0300

    docs: high-frequency-small-interval? doc

--- src/authorizer/rules.clj
+++ src/authorizer/rules.clj
@@ -7,13 +7,13 @@
   (true? (>= (:limit account) transaction-amount)))
 
 (defn active-card?
-  "return true if a card is active
-   otherwhise false"
+  "return true if a card is active otherwhise false"
    [account]
    (true? (:active-card account)))
 
 (defn high-frequency-small-interval?
   [transactions]
+  "return true if there are 3 or more transactions on a 2 minute interval"
   (let [interval-in-minutes 2
         high-frequency-transaction-count 3
         date-time-start (t/minus (t/now) (t/minutes interval-in-minutes))]
